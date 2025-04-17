365a234a83e6c7ed2e39edba59640459fa9165d9
ysemko <ysexdr@protonmail.com>
2025-04-17 23:22:39 +0200

    feat: added grafana dashboard

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,8 @@ AppDataSource.initialize().then(async () => {
         activeCountry: 0,
         activeCountryId: "",
         newNoticeCount: 0,
-        failedRequests: 0
+        failedRequests: 0,
+        requests: 0,
     };
 
     try {
@@ -28,32 +29,36 @@ AppDataSource.initialize().then(async () => {
 
         const countries: Country[] = await AppDataSource.manager
             .createQueryBuilder(Country, "country")
-            .distinctOn(["country.country_id"]).orderBy("country.country_id", "DESC")
+            .distinctOn(["country.country_id"]).orderBy("country.country_id", "ASC")
             .getMany();
 
-            stats.countCountries = countries.length;
+        stats.countCountries = countries.length;
 
         for (const country of countries) {
             stats.activeCountry = countries.findIndex(c => c.country_id === country.country_id);
             stats.activeCountryId = country.country_id;
 
-            for (let age = 0; age < 100; age += 10) {
+            for (let age = 10; age < 90; age += 1) {
                 //Males
-                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age + 10, ageMin: age, sexId: "M" });
-                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "M" });
-                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "M" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, nationality: country.country_id, ageMax: age, ageMin: age, sexId: "M" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age, ageMin: age, sexId: "M" });
+                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "M" });
+                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "M" });
                 //Females
-                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age + 10, ageMin: age, sexId: "F" });
-                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "F" });
-                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "F" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, nationality: country.country_id, ageMax: age, ageMin: age, sexId: "F" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age, ageMin: age, sexId: "F" });
+                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "F" });
+                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "F" });
                 //Unknown
-                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age + 10, ageMin: age, sexId: "U" });
-                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "U" });
-                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age, sexId: "U" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, nationality: country.country_id, ageMax: age, ageMin: age, sexId: "U" });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age, ageMin: age, sexId: "U" });
+                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "U" });
+                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age, sexId: "U" });
                 // NULL
-                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age + 10, ageMin: age });
-                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age });
-                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age + 10, ageMin: age });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, nationality: country.country_id, ageMax: age, ageMin: age });
+                await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id, ageMax: age, ageMin: age });
+                await fetchRedNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age });
+                await fetchYellowNotices(stats, { nationality: country.country_id, ageMax: age, ageMin: age });
             }
 
             await fetchRedNotices(stats, { arrestWarrantCountryId: country.country_id });
@@ -76,7 +81,7 @@ async function fetchRedNotices(stats: any, params = {}): Promise<number> {
     let noticeEntities: RedNoticeDetailsEntityDB[] = [];
     try {
         const redNotices: RedNoticesEntity = await InterpolService.getRedNotices({ resultPerPage: 200, page: 1, ...params });
-
+        stats.requests++;
         for (const notice of redNotices._embedded.notices) {
             if (await AppDataSource.getRepository(RedNoticeDetailsEntityDB).findOne({ where: { entity_id: notice.entity_id } })) {
                 return
@@ -122,13 +127,16 @@ async function fetchYellowNotices(stats: any, params = {}): Promise<number> {
 
 function backgroundLogs(stats: any, start) {
     loggingIntervalId = setInterval(() => {
+        console.clear();
         console.log("----------------------------------------");
         console.log("Elapsed time: " + (Date.now() - start) / 1000 + " seconds");
+        console.log("Total requests made: " + stats.requests);
+        console.log("requests per second: " + (stats.requests / ((Date.now() - start) / 1000)).toFixed(4));
         console.log("Current country: " + stats.activeCountryId);
         console.log("New notices fetched: " + stats.newNoticeCount);
         console.log("Failed requests: " + stats.failedRequests);
         console.log("Countries processed: " + stats.activeCountry + "/ " + stats.countCountries + " (" + (stats.activeCountry / stats.countCountries * 100).toFixed(2) + "%)");
-        console.log("Estimated time left: " + ((Date.now() - start) / stats.activeCountry) * (stats.countCountries - stats.activeCountry) / 1000 + " seconds");
+        console.log("Estimated time left: " + (((Date.now() - start) / stats.activeCountry) * (stats.countCountries - stats.activeCountry) / 1000).toFixed(0) + " seconds");
         console.log("----------------------------------------");
     }, 5000);
 }
